6daa5c1e75c23c7d1f8654f09f27e009d1763cbd
TsuyoshiTsai <patrick3948@gmail.com>
2025-03-13 20:26:28 +0800

    feat: add wording, styles and RWD

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
 // import { useState } from "react";
 import "./App.css";
 
-function App() {
+const Wordings = "Team Taiwan! Team Taiwan! Taiwan is a great country!";
 
+function App() {
   return (
-    <>
-      <picture class="cases-background">
+    <main className="main">
+      <div className="quote-text">{Wordings}</div>
+
+      <picture className="background">
         <source
           data-srcset="https://dogstudio.co/app/themes/portfolio-2018/static/assets/images/cases/background-xxs.png"
-          media="(max-width: 450px)"
+          media="(max-width: 30em)"
           srcset="https://dogstudio.co/app/themes/portfolio-2018/static/assets/images/cases/background-xxs.png"
         />
         <source
           data-srcset="https://dogstudio.co/app/themes/portfolio-2018/static/assets/images/cases/background-m.png"
-          media="(max-width: 815px)"
+          media="(max-width: 48em)"
           srcset="https://dogstudio.co/app/themes/portfolio-2018/static/assets/images/cases/background-m.png"
         />
         <source
           data-srcset="https://dogstudio.co/app/themes/portfolio-2018/static/assets/images/cases/background-l.png"
-          media="(max-width: 1030px)"
+          media="(max-width: 64em)"
           srcset="https://dogstudio.co/app/themes/portfolio-2018/static/assets/images/cases/background-l.png"
         />
         <img
           data-src="https://dogstudio.co/app/themes/portfolio-2018/static/assets/images/cases/background-xl.png"
-          class="js-lazy loaded"
           alt=""
           src="https://dogstudio.co/app/themes/portfolio-2018/static/assets/images/cases/background-xl.png"
           data-was-processed="true"
         />
       </picture>
-    </>
+    </main>
   );
 }
 
