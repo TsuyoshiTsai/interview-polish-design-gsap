2607e0ab0ac46934d3e66ab59e31ca9e4f9d66ff
TsuyoshiTsai <patrick3948@gmail.com>
2025-03-14 00:02:54 +0800

    feat: add gsap animation well done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
-// import { useState } from "react";
-import "./App.css";
+import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import "./App.scss";
 
-const Wordings = "Team Taiwan! Team Taiwan! Taiwan is a great country!";
+gsap.registerPlugin(useGSAP);
 
+const Letter = (props) => (
+  <span className="quote-text__letter" data-quote-text-letter {...props} />
+);
+
+const quoteText = "Team Taiwan! Team Taiwan! Taiwan is a great country!"
+  .split(" ")
+  .map((word, index, words) => (
+    <React.Fragment key={`${word}-${index}`}>
+      {index !== 0 && <>&nbsp;</>}
+      {index === 0 && <Letter>“</Letter>}
+
+      {/* NOTE: add a word container to avoid breaking words */}
+      <span className="quote-text__word">
+        {word.split("").map((letter, index) => (
+          <Letter key={`${letter}-${index}`}>{letter}</Letter>
+        ))}
+      </span>
+
+      {index === words.length - 1 && <Letter>“</Letter>}
+    </React.Fragment>
+  ));
+
+console.log("quoteText", quoteText);
 function App() {
+  useGSAP(() => {
+    gsap.from("[data-quote-text-letter]", {
+      x: 80,
+      y: 50,
+      z: -300,
+      scaleY: 0.1,
+      // NOTE: use transform property to specify rotation order
+      transform: "rotateX(-90deg) rotate(-35deg)",
+      opacity: 0,
+
+      ease: "back.out",
+      duration: 0.8,
+      stagger: 0.015,
+    });
+  });
+
   return (
     <main className="main">
-      <div className="quote-text">{Wordings}</div>
+      <div className="quote-text">{quoteText}</div>
 
       <picture className="background">
         <source
